0beb82e1e5781ab55e6a529228732ac26a46f681
egoing <egoing@gmail.com>
2023-01-24 11:10:34 +0900

    Component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import styles from '@/styles/Home.module.css'
+function Counter(){
+  return <p>
+    0 <input type="button" value="+"/>
+  </p>
+}
 export default function Home() {
   return (
     <>
       <h1>Counter</h1>
-      0 <input type="button" value="+"/>
+      <Counter></Counter>
+      <Counter></Counter>
     </>
   )
 }
