6ff274fb68dee9111f296bb9f09cf55c42b40811
egoing <egoing@gmail.com>
2023-01-24 21:12:52 +0900

    controlled component

--- components/Counter.js
+++ components/Counter.js
@@ -1,10 +1,15 @@
 import React, {useState} from 'react';
 export function Counter({down}) {
   const [count, setCount] = useState(0);
-  const upHandler = ()=>setCount(oldCount=>oldCount+1);
-  const downHandler = ()=>setCount(oldCount=>oldCount-1);
+  const [step, setStep] = useState(1);
+  const upHandler = ()=>setCount(oldCount=>oldCount+step);
+  const downHandler = ()=>setCount(oldCount=>oldCount-step);
   return <p>
-    {count} <input type="button" value="+" onClick={upHandler} />
+    {count} 
+    <input type="number" value={step} onChange={evt=>{
+      setStep(Number(evt.target.value));
+    }} />
+    <input type="button" value="+" onClick={upHandler} />
     {down ? <input type="button" value="-" onClick={downHandler} /> : null}
   </p>;
 }
