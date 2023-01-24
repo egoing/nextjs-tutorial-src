2d86f385496c00d7f1563ca497fe5055da73ff29
egoing <egoing@gmail.com>
2023-01-24 22:01:03 +0900

    useEffect & Ajax

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import styles from '@/styles/Home.module.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Counter } from '../components/Counter'
 import { CounterDummy } from '../components/CounterDummy'
 export default function Home() {
   const [count, setCount] = useState(10);
+  useEffect(()=>{
+    fetch('/api/count')
+      .then(res=>res.json())
+      .then(result=>{
+        setCount(result.value);
+      })
+  },[])
   return (
     <>
       <h1>Counter</h1>
-      <Counter></Counter>
       {count} <CounterDummy value={count} onChangeValue={(newCount)=>{
         setCount(newCount);
       }}></CounterDummy>
