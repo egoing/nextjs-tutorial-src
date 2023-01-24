4443546d1fe2e04486eead3b32df046d4f23e6e6
egoing <egoing@gmail.com>
2023-01-24 11:50:49 +0900

    useStae callback function

--- components/Counter.js
+++ components/Counter.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 export function Counter({down}) {
   const [count, setCount] = useState(0);
-  const upHandler = ()=>setCount(count+1);
-  const downHandler = ()=>setCount(count-1);
+  const upHandler = ()=>setCount(oldCount=>oldCount+1);
+  const downHandler = ()=>setCount(oldCount=>oldCount-1);
   return <p>
     {count} <input type="button" value="+" onClick={upHandler} />
     {down ? <input type="button" value="-" onClick={downHandler} /> : null}
